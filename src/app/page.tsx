258e1dcbaed436ecaffd6f4cc1b798a17aa111ba
Alberto García <albertoinformatica97@gmail.com>
2023-11-18 23:50:53 +0100

    added reset form tailwind package. home page. create-pages and page-chip components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { CreatePages } from '@/components/create-pages/create-pages'
+
 export default function Home() {
-	return <h1 className='text-center'>Hello Sandscripters! ❤️</h1>
+	return (
+		<div className='w-full sm:w-1/2 md:w-2/5 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+			<div className='flex flex-col gap-2 p-4'>
+				<h1>Una web sobre...</h1>
+				<input type='text' className='w-full' placeholder='Casas prefabricadas' />
+				<CreatePages />
+			</div>
+		</div>
+	)
 }
